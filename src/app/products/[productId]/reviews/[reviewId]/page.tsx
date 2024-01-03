5ab9267d700a822ef9c60e399d1ee2056df9a7e2
Akshay8122 <akshay.p@simformsolutions.com>
2024-01-03 16:38:46 +0530

    feat: added nested dynamic routes

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,11 @@
+export default function Reviews({
+  params,
+}: {
+  params: { productId: string; reviewId: string };
+}) {
+  return (
+    <h1>
+      Review {params.reviewId} for product {params.productId}
+    </h1>
+  );
+}
